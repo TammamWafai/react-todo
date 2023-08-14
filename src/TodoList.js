9eb1b11db7a53dc1b8727108bbdcf29812110333
TammamWafai <tammam.wafai@gmail.com>
2023-08-14 00:38:06 -0500

    Lesson-1.8

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-
+function dynamicSort(property) {
+    return function (a, b) {
+        return (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+    }
+}
 
 function TodoList({ todoList, onRemoveTodo }) {
+
     return (
         <>
 
