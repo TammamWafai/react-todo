3be438312344b111989901ad6838bfcd456e3024
Tammam <tammam.wafai@gmail.com>
2023-09-04 19:35:28 -0500

    lesson-3.1

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,37 +1,35 @@
-import React from 'react';
+import React from "react";
+import style from "./TodoListItem.module.css";
 
 const TodoListItem = ({ id, todo, onRemoveTodo }) => {
-    const handleRemoveClick = async () => {
-        const options = {
-            method: 'DELETE',
-            headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
-            },
-        };
-
-        const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/${id}`;
-
-        try {
-            const response = await fetch(url, options);
-
-            if (!response.ok) {
-                throw new Error(`Error: ${response.status}`);
-            }
-
-            onRemoveTodo(id); // Remove the item from the local state
-        } catch (error) {
-            console.error(error.message);
-        }
+  const handleRemoveClick = async () => {
+    const options = {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_KEY}`,
+      },
     };
 
-    return (
-        <li key={id}>
-            {todo}{' '}
-            <button onClick={handleRemoveClick}>
-                Remove
-            </button>
-        </li>
-    );
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/${id}`;
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      onRemoveTodo(id); // Remove the item from the local state
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <li key={id} className={style.ListItem}>
+      {todo} <button onClick={handleRemoveClick}>Remove</button>
+    </li>
+  );
 };
 
 export default TodoListItem;
